00ef4be8bee635a2b19b559bcaff53e6d586d167
Eduardo Brito Sanchez <eduardo_brito27@hotmail.com>
2014-10-23 12:00:29 +0100

    Agregada la condicion de existir las respuestas

--- spec/exam_spec.rb
+++ spec/exam_spec.rb
@@ -9,5 +9,8 @@ describe Examen do
 		it "Se crea la pregunta?" do
 			@examen.question.should eq("Que hora es?")
 		end
+		it "Se crean las respuestas?"
+			@arrayprueba = ['1','2','3','4']
+			@examen.respuestas.to_a == @hashprueba;
 	end
 end
