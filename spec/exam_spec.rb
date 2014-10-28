8acfd8148100511295b053d25eb913c3ab2b23de
Eduardo Brito Sanchez <eduardo_brito27@hotmail.com>
2014-10-28 17:10:22 +0000

    Terminada la clase lista v2

--- spec/exam_spec.rb
+++ spec/exam_spec.rb
@@ -4,8 +4,6 @@ require 'exam'
 describe Examen do
 	before :each do
 		@ex= Examen.new("\nQue hora es?",['11:30','2:20','3:55','4:58'])
-		@node = Nodo.new(1,2)
-		@list = List.new
 	end
 	describe "Pruebas de inicializacion" do
 		it "Se crea la pregunta?" do
@@ -28,22 +26,30 @@ describe Examen do
 			@ex.printex
 		end
 	end
-	describe "Pruebas de listas\n" do
-		it "No existe un nodo" do
-			@node.is_a?(Nodo)
-		end
-		it "Se extrae el primer elemento" do
-			@list.shift
-		end
-		it "Se inserta un elemento" do
-			@list.push(@node)
-		end
-		it "Se pueden insertar varios elementos" do
-			newArray = [Nodo.new(1,3),Nodo.new(3,7)]
-			@list.multipush(newArray)
-		end
-		it "Debe existir una lista con su cabeza" do
-			@list.head.is_a?(Nodo)
-		end
-	end	
+
+	describe "Pruebas de preguntas de practica 6" do
+	end
 end
+
+describe List do
+	before :each do
+		@node = Nodo.new(1,2)
+		@list = List.new
+	end
+	it "No existe un nodo" do
+		@node.is_a?(Nodo)
+	end
+	it "Se extrae el primer elemento" do
+		@list.shift
+	end
+	it "Se inserta un elemento" do
+		@list.push(@node)
+	end
+	it "Se pueden insertar varios elementos" do
+		newArray = [Nodo.new(1,3),Nodo.new(3,7)]
+		@list.multipush(newArray)
+	end
+	it "Debe existir una lista con su cabeza" do
+		@list.head.is_a?(Nodo)
+	end
+end	
